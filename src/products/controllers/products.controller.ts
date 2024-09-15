845caff0fb9755f465a0fc61ee888d413ae0dce9
Kevin Velásquez <kevin.vela519@gmail.com>
2024-09-15 11:48:43 -0600

    feat: add endpoint logic to remove product

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Delete, Get, HttpCode, HttpStatus, Param, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ProductsService } from '../services/products.service';
 import { ProductFilterQueryDto } from '../dtos/product-filter-query.dto';
 import { ProductPaginatedResultDto } from '../dtos/product-paginated-result.dto';
+import { IdUuidParamDto } from '../../core/dtos/id-uuid-param.dto';
 
 @Controller('products')
 @ApiTags('Products')
@@ -12,5 +13,11 @@ export class ProductsController {
   @Get()
   listProductsPage(@Query() filterQueryDto: ProductFilterQueryDto): Promise<ProductPaginatedResultDto> {
     return this.productsService.listProductsPage(filterQueryDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  removeProductById(@Param() idParam: IdUuidParamDto) {
+    return this.productsService.removeProductById(idParam.id);
   }
 }
